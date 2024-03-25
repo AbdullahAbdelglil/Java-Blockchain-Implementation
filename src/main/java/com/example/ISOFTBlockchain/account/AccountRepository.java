374a976fb17ca663d33e19cdb23f325fe9b27db4
Abdullah.Zaky <abdoullahzaky1@gmail.com>
2024-03-25 10:01:08 +0200

    initial commit

--- src/main/java/com/example/ISOFTBlockchain/account/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ISOFTBlockchain.account;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends MongoRepository<Account, String> {
-    Account getAccountByAccountNumber(String accountNumber);
-}
